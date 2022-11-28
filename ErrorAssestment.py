285e51f36aa63456991fc4f10caf910f940880c8
Huy Luu <huyluu1820@gmail.com>
2022-11-28 21:50:59 +0700

    Able to append the segmments However missing the last segment

--- ErrorAssestment.py
+++ ErrorAssestment.py
@@ -1,9 +1,11 @@
 from FileReader import FileReader
 from Point import Point
+from Point import OriginalPoint
 
 path = "E:\\New folder\\BK\\HK221\\Luan_van_tot_nghiep\\Temporary\\data\\test.txt"
 
 og_points = []
+positions = []
 
 data = FileReader.readFromText(path)
 #print(data)
@@ -29,15 +31,22 @@ for i in range(0, len(og_points)):
     print(og_points[i].x, og_points[i].y)
 
 # Cart positions appending
-for i in range(0, len(ps_indices)):
+for i in range(0, len(ps_indices)-1):
     if(i == 0):
-        for j in range(start_position_set + 1, ps_indices[i]-1):
-            print(data[j])
+        for j in range(start_position_set + 1, start_position_set +1 + ps_indices[i] - 1):
+            #print(data[j])
+            positions.append(OriginalPoint(data[j]))
 
     else:
-        for j in range(ps_indices[i-1], ps_indices[i]):
-            print(data[j])
+        for j in range(ps_indices[i-1] + 1, start_position_set + 1 + ps_indices[i] - 1):
+            #print(data[j])
+            positions.append(OriginalPoint(data[j]))
 
 
+
+for i in range(0, len(positions)):
+    print(str(positions[i].getLat()) + " " + str(positions[i].getLon()))
+
+print(len(positions))
 
 
